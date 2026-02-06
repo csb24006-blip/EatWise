24eac69271d5576578f05333da3f264df4c01254
Nilav Bhujel <nilavbhujel@Nilavs-MacBook-Air.local>
2026-02-07 03:25:41 +0530

    fix: add base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// export default defineConfig({
-//   base: '/EatWiseAI/', // Replace with your exact Repo Name
-//   plugins: [react()],
-// })
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // This ensures your assets (JS/CSS) load correctly from the subfolder
+  base: '/EatWise/', 
   plugins: [react()],
 })
